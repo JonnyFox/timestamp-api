0b5a7bac9daaeba7a689c7fb640ee84e6711668f
Jonny Fox <fox.jonny@gmail.com>
2016-11-03 23:54:30 +0000

    done!

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const express = require('express');
+const datejs = require('datejs');
 var app = express();
 
-app.get('/', (req, res)=>{
-    res.send('Hello world');
-});
+function isEmpty(obj){
+    return Object.keys(obj).length === 0 && obj.constructor === Object;
+}
 
-console.log(process.env.PORT);
-console.log(process.env.IP);
+app.get('/:value', (req, res) => {
+    var query = req.params.value;
+    var result = {
+        unix: null,
+        natural: null
+    };
+    var date = null;
+    if (!isEmpty(query)) {
+        if (isNaN(query)) {
+            date = new Date(Date.parse(query));
+        }
+        else {
+            date = new Date(+query*1000);
+        }
+        
+        if(date && date.getTime() != 0){
+            result.unix = date.getTime()/1000|0;
+            result.natural = date.toString('MMMM d, yyyy');
+        }
+    }
+    
+    res.json(result);
+});
 
 
 app.listen(process.env.PORT, process.env.IP);
